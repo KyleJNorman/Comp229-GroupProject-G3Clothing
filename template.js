35b061729879425eba9f00c86b653419726e3fb8
Kyle Norman <kyle.norman.gs3@gmail.com>
2023-11-12 16:25:43 -0500

    pass 3 of uploading assignment 2

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => { 
+    return `<!doctype html>
+    <html lang="en"> 
+    <head>
+    <meta charset="utf-8">
+    <title>Assigment 2</title> 
+    </head>
+    <body>
+    <div id="root">Welcome to the Products page!</div> 
+    </body>
+    </html>`
+ }
