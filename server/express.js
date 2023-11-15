5f62c8ad49f88d222134dd45afac21555da9b80c
Kyle Norman <kyle.norman.gs3@gmail.com>
2023-11-15 07:19:34 -0500

    update express.js to test

--- server/express.js
+++ server/express.js
@@ -1,3 +1,4 @@
+/*
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
@@ -29,7 +30,7 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
-/*
+
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
     res.status(401).json({"error" : err.name + ": " + err.message}) 
@@ -38,5 +39,36 @@ app.use((err, req, res, next) => {
     console.log(err)
     } 
     })
-    */
+    
+export default app
+*/
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from './../template.js'
+import productRoutes from './routes/product.routes.js'
+import userRoutes from './routes/users.routes.js'
+
+
+const app = express()
+
+//...
+app.get('/', (req, res) => {
+res.status(200).send(Template()) 
+})
+//...
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', productRoutes)
+app.use('/', userRoutes)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
 export default app
